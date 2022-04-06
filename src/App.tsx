cb275ea0dd786c45a6d25d58c101999935d35af1
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-06 13:24:31 -0500

    create WordRow, remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import WordRow from "./WordRow";
 
-function App() {
+export default function App() {
   return (
     <div className="mx-auto w-96">
-      <h1 className="text-4xl text-center">Wordle</h1>
+      <header className="border-b border-gray-500 pb-2 my-2">
+        <h1 className="text-4xl text-center">Wordle</h1>
+      </header>
+
+      <main>
+        <WordRow letters="hello" />
+        <WordRow letters="hello" />
+        <WordRow letters="hello" />
+        <WordRow letters="hello" />
+        <WordRow letters="hello" />
+        <WordRow letters="hello" />
+      </main>
     </div>
   );
 }
-
-export default App;
