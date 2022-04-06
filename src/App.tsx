11db18c912dbe473944253306dd6e65f20f8dd1a
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-06 12:05:35 -0500

    Add work bank and basic random word test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="mx-auto w-96">
       <h1 className="text-4xl text-center">Wordle</h1>
