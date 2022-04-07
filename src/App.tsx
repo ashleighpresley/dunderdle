0e39abfdf63027231212dd253418c898c0e4225e
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-06 23:49:58 -0500

    show New Game modal when all guesses are complete

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,12 @@ export default function App() {
   }
 
   const numGuessesRemaining = GUESS_LENGTH - rows.length;
+  const isGameOver = state.guesses.length === GUESS_LENGTH;
 
   rows = rows.concat(Array(numGuessesRemaining).fill(""));
 
   return (
-    <div className="mx-auto w-96">
+    <div className="mx-auto w-96 relative">
       <header className="border-b border-gray-500 pb-2 my-2">
         <h1 className="text-4xl text-center">Wordle</h1>
         <div>
@@ -38,6 +39,7 @@ export default function App() {
             className="w-full p-2 border-2 border-gray-500"
             value={guess}
             onChange={onChange}
+            disabled={isGameOver}
           />
         </div>
       </header>
@@ -47,6 +49,24 @@ export default function App() {
           <WordRow key={index} letters={word} />
         ))}
       </main>
+
+      {isGameOver && (
+        <div
+          role="modal"
+          className="absolute bg-white left-0 right-0 top-1/4 p-6 text-center w-3/4 mx-auto rounded border border-gray-800"
+        >
+          Game Over!
+          <button
+            className="block border rounded border-green-500 bg-green-500 p-2 mt-4 mx-auto shadow"
+            onClick={() => {
+              state.newGame();
+              setGuess("");
+            }}
+          >
+            Play Again
+          </button>
+        </div>
+      )}
     </div>
   );
 }
