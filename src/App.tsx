39e936f6fd34cba1d65ffd6fa8bdd2fe0fa2077f
ashleighpresley <ashleighpresley1@gmail.com>
2022-04-07 01:15:29 -0500

    Move result calculation into store

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ export default function App() {
     setGuess(newGuess);
   };
 
-  let rows = [...state.guesses];
+  let rows = [...state.rows];
 
   if (rows.length < GUESS_LENGTH) {
-    rows.push(guess);
+    rows.push({ guess });
   }
 
   const numGuessesRemaining = GUESS_LENGTH - rows.length;
-  const isGameOver = state.guesses.length === GUESS_LENGTH;
+  const isGameOver = state.rows.length === GUESS_LENGTH;
 
   rows = rows.concat(Array(numGuessesRemaining).fill(""));
 
@@ -45,8 +45,8 @@ export default function App() {
       </header>
 
       <main className="grid grid-rows-6 gap-4">
-        {rows.map((word, index) => (
-          <WordRow key={index} letters={word} />
+        {rows.map(({ guess, result }, index) => (
+          <WordRow key={index} letters={guess} result={result} />
         ))}
       </main>
 
